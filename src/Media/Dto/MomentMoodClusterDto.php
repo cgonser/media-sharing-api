8382ac351d3a44fb412ce631b0a87e2c4462fe97
Carlos Gonser <carlos@itinair.com>
2022-05-15 22:12:22 +0200

    fix api docs

--- src/Media/Dto/MomentMoodClusterDto.php
+++ src/Media/Dto/MomentMoodClusterDto.php
@@ -2,10 +2,14 @@
 
 namespace App\Media\Dto;
 
+use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+
 class MomentMoodClusterDto
 {
     public string $mood;
 
     /** @var array MomentMoodDto[] */
+    #[OA\Property(type: "array", items: new OA\Items(ref: new Model(type: MomentMoodDto::class)))]
     public array $moments = [];
 }
--- src/Media/Dto/MomentMoodMapDto.php
+++ src/Media/Dto/MomentMoodMapDto.php
@@ -2,11 +2,16 @@
 
 namespace App\Media\Dto;
 
+use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+
 class MomentMoodMapDto
 {
     /** @var array MomentMoodDto[] */
+    #[OA\Property(type: "array", items: new OA\Items(ref: new Model(type: MomentMoodDto::class)))]
     public array $moments = [];
 
-    /** @var array MomentMoodCluster[] */
+    /** @var array MomentMoodClusterDto[] */
+    #[OA\Property(type: "array", items: new OA\Items(ref: new Model(type: MomentMoodClusterDto::class)))]
     public array $clusters = [];
 }
